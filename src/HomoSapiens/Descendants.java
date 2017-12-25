6c76b5d3724ec9ec77f63dd503072039387fc540
drarifsun <drarifsun@gmail.com>
2017-12-25 12:33:46 -0500

    Preface added

--- src/HomoSapiens/Descendants.java
+++ src/HomoSapiens/Descendants.java
@@ -1,9 +1,16 @@
 package HomoSapiens;
 
 public class Descendants {
-
+/*
+ * History of human evaluation
+ * Author: Dr. Arif
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("The evolution of human is a topic of major interst\n");
+		System.out.print("\nToday's modern human once was living under the sky \n");
+		System.out.println("After the big bang, life form began to sprout on earth");
+		System.out.println("with many forms including primitive human called apes");
 		System.out.println("Modern Human are the descendants of Apes");
 		System.out.println("This theory was given by Charles Darwin");
 		System.out.println("the father of Genesis");
